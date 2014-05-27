a11d700d211aa7df9b1b58d3cf85d34daad3b1b2
Jorgen Larsen de Martino <jolde@gst.dk>
2014-05-27 15:32:27 +0200

    Tilføjet 2 zoom levels til WMTS

--- examples/apps/app1/js/FindStedet.Layers.js
+++ examples/apps/app1/js/FindStedet.Layers.js
@@ -1,21 +1,21 @@
 findStedet_layers = [
 { layerid: 1, type: "WMTS", name: "Sk\u00E6rmkort", url: "http://kortforsyningen.kms.dk/topo_skaermkort", params: { service: 'WMTS', version: '1.0.0',
     request: 'GetTile', format: 'image/jpeg', layer: 'dtk_skaermkort', style: 'default', BGCOLOR: '0xDCF0F9',
-    matrixSet: 'View1', matrixIds: 'L00,L01,L02,L03,L04,L05,L06,L07,L08,L09,L10,L11', info_format: 'text/plain',
+    matrixSet: 'View1', matrixIds: 'L00,L01,L02,L03,L04,L05,L06,L07,L08,L09,L10,L11,L12,13', info_format: 'text/plain',
     transparent: true, ticket: ""
 }, options: { isBaseLayer: true, visibility: false, requestEncoding: 'kvp',layerid: 1 }
 }
 ,
 { layerid: 2, type: "WMTS", name: "Flyfoto", url: "http://kortforsyningen.kms.dk/orto_foraar", params: { service: 'WMTS', version: '1.0.0',
     request: 'GetTile', format: 'image/jpeg', layer: 'orto_foraar', style: 'default', BGCOLOR: '0xDCF0F9',
-    matrixSet: 'View1', matrixIds: 'L00,L01,L02,L03,L04,L05,L06,L07,L08,L09,L10,L11', info_format: 'text/plain',
+    matrixSet: 'View1', matrixIds: 'L00,L01,L02,L03,L04,L05,L06,L07,L08,L09,L10,L11,L12,L13', info_format: 'text/plain',
     transparent: true, ticket: ""
 }, options: { isBaseLayer: true, visibility: false, requestEncoding: 'kvp' , layerid: 2 }
 },
 
 { layerid: 4, type: "WMTS", name: "Hill shade", url: "http://kortforsyningen.kms.dk/dhm", params: { service: 'WMTS', version: '1.0.0',
     request: 'GetTile', format: 'image/jpeg', layer: 'hillshade_1_6m', style: 'default', BGCOLOR: '0xDCF0F9',
-    matrixSet: 'View1', matrixIds: 'L00,L01,L02,L03,L04,L05,L06,L07,L08,L09,L10,L11', info_format: 'text/plain',
+    matrixSet: 'View1', matrixIds: 'L00,L01,L02,L03,L04,L05,L06,L07,L08,L09,L10,L11,L12,L13', info_format: 'text/plain',
     transparent: true, ticket: ""
 }, options: { isBaseLayer: false, visibility: false, opacity: 0.3, requestEncoding: 'kvp' , layerid: 4 }
 },
