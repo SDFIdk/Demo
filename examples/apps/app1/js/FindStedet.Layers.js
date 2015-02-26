d8f811839a241c43c146dbba6b91657f6ea68082
Jorgen Larsen de Martino <jorgen@demartino.dk>
2015-02-26 12:32:25 +0100

    Bug fix to Skaermkort daempet

--- examples/apps/app1/js/FindStedet.Layers.js
+++ examples/apps/app1/js/FindStedet.Layers.js
@@ -6,7 +6,7 @@ findStedet_layers = [
 }, options: { isBaseLayer: true, visibility: false, requestEncoding: 'kvp',layerid: 1 }
 },
 
-{ layerid: 5, type: "WMTS", name: "Sk\u00E6rmkort", url: "http://kortforsyningen.kms.dk/topo_skaermkort_daempet", params: { service: 'WMTS', version: '1.0.0',
+{ layerid: 5, type: "WMTS", name: "Sk\u00E6rmkort d\u00E6mpet", url: "http://kortforsyningen.kms.dk/topo_skaermkort_daempet", params: { service: 'WMTS', version: '1.0.0',
     request: 'GetTile', format: 'image/jpeg', layer: 'dtk_skaermkort_daempet', style: 'default', BGCOLOR: '0xDCF0F9',
     matrixSet: 'View1', matrixIds: 'L00,L01,L02,L03,L04,L05,L06,L07,L08,L09,L10,L11,L12,L13', info_format: 'text/plain',
     transparent: true, ticket: ""
