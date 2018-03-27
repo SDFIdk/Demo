6bf5ffca76eabc176784e83cce70d190e5105b8a
Jørgen Larsen De Martino <B004158@PROD.SITAD.DK>
2018-03-27 12:06:18 +0200

    Oprydning i eksempler
    Blandt andet er apps bygget på OL2 er fjernet

--- examples/apps/app1/js/defs/EPSG25832.js
+++
@@ -1,1 +0,0 @@
-Proj4js.defs["EPSG:25832"] = "+title=  ETRF89 / UTM zone 32N EPSG:25832 +proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs";
--- server/Release_IP/releaseIP_webpage.php
+++
@@ -1,9 +0,0 @@
-	// 
-	// Replace the XXXXX login information with your own login
-	// Releases the IP for the server or work station making the request and gives 24 hours access to Kortforsyningen, using your organization's login
-
-	//If you are using a GIS application on a workstation please visit www.kortforsyningen.dk and use the Frigør IP function on the frontpage
-
-<?php 
-	drupal_add_js('jQuery(document).ready(function () { jQuery.ajax({url: 	"https://services.kortforsyningen.dk/?request=UnlockIP&login=XXXXX&password=XXXXX", type: "GET", crossDomain: true});})' , 'inline');
-?>
