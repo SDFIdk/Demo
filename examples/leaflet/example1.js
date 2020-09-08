6bad1a3257358e2bae8e861e7aa0c74dfa8be0ad
Gebuz <johanln@hotmail.dk>
2020-09-08 12:12:08 +0200

    remove workaround since we aint using fractional zoom levels.

--- examples/leaflet/example1.js
+++ examples/leaflet/example1.js
@@ -1,20 +1,5 @@
 (function() {
 
-    //  Workaround for 1px lines appearing in some browsers due to fractional transforms
-    //  and resulting anti-aliasing.
-    //  https://github.com/Leaflet/Leaflet/issues/3575
-    if (window.navigator.userAgent.indexOf('Chrome') > -1) {
-        var originalInitTile = L.GridLayer.prototype._initTile;
-        L.GridLayer.include({
-            _initTile: function (tile) {
-                originalInitTile.call(this, tile);
-                var tileSize = this.getTileSize();
-                tile.style.width = tileSize.x + 1 + 'px';
-                tile.style.height = tileSize.y + 1 + 'px';
-            }
-        });
-    }
-
     // Set Kortforsyningen token, replace with your own token
     var kftoken = 'd12107f70a3ee93153f313c7c502169a';
 
