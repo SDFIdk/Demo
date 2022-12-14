09f5f9c863b92b5614bd64906a2c8ef5c7baef0a
Frank Thomsen <frtho@sdfi.dk>
2022-12-14 14:12:42 +0100

    fix leaflet zoom bug

--- src/leaflet.js
+++ src/leaflet.js
@@ -4,5 +4,4 @@ const translator = createTranslator('EPSG:25832', 'WGS84')
 
 export { default as proj4 } from 'proj4';
 export { GSearchUI } from '@dataforsyningen/gsearch-ui';
-
 export { translator } 
