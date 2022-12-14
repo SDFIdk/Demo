2e1d5b601b5e5f6f158ca242ddf63599cc251fa0
Frank Thomsen <frtho@sdfi.dk>
2022-12-14 13:14:18 +0100

    update leaflet example 3

--- src/leaflet.js
+++ src/leaflet.js
@@ -1,2 +1,8 @@
+import { createTranslator } from '../../saul/modules/saul-projection';
+
+const translator = createTranslator('EPSG:25832', 'WGS84')
+
 export { default as proj4 } from 'proj4';
 export { GSearchUI } from '@dataforsyningen/gsearch-ui';
+
+export { translator } 
